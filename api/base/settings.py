a0c8fe712edd06707beb8b651570d3cb6eb2e48c
nightwarriorftw <amanv1999@gmail.com>
2022-10-04 18:56:29 +0530

    Add db port to configurations

--- api/base/settings.py
+++ api/base/settings.py
@@ -95,6 +95,7 @@ DATABASES = {
     'NAME': os.environ.get('DB_NAME'),
     'USER': os.environ.get('DB_USER'),
     'PASSWORD': os.environ.get('DB_PASSWORD'),
+    'PORT': os.environ.get('DB_PORT', '5432'),
   }
 }
 
