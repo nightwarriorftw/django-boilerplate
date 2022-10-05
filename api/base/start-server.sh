2cbbfb2ca607f485c2fdf15064e239647c4bd3c1
nightwarriorftw <amanv1999@gmail.com>
2022-10-05 13:55:56 +0530

    Add gunicorn and its heuristics for running production servers

--- api/base/start-server.sh
+++ api/base/start-server.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-exec python manage.py runserver 0.0.0.0:8000
+if [ $DEBUG="False" ]; then
+    echo "Starting the production server !!"
+    exec gunicorn --bind 0.0.0.0:8000 base.wsgi
+else
+    echo "Starting the development server !!"
+    exec python manage.py runserver 0.0.0.0:8000
+fi
 
 exit 0
