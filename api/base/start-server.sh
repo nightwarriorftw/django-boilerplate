179b9af33290edc7b8b43a90c37778098a8d2df4
nightwarriorftw <amanv1999@gmail.com>
2022-10-08 19:15:20 +0530

    Fix start server conditions

--- api/base/start-server.sh
+++ api/base/start-server.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [ $DEBUG="False" ]; then
-    echo "Starting the production server !!"
-    exec gunicorn --bind 0.0.0.0:8000 base.wsgi
-else
+if [[ "$DEBUG" == "True" ]]; then
     echo "Starting the development server !!"
     exec python manage.py runserver 0.0.0.0:8000
+else
+    echo "Starting the production server !!"
+    exec gunicorn --bind 0.0.0.0:8000 base.wsgi
 fi
 
 exit 0
